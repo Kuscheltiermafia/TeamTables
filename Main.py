e172603524fe71c2487114106bc4b6df573da977
Ninjarenz <144927362+Ninjarenz@users.noreply.github.com>
2025-10-05 21:12:17 +0200

    creating in ./tests an test for the SQL dataabase using DuckDB

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 from flask import *
 import requests
-import markdown
 
 app = Flask(__name__)
 
@@ -9,8 +8,6 @@ README_URL = f"https://github.com/Kuscheltiermafia/TeamCard/blob/main/README.md"
 @app.route('/')
 def index():
     return render_template("index.html")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
