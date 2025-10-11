1657e825cf97988d864fce24221a3a7869143b19
Morgon <moritz.olthoff@gmail.com>
2025-10-11 11:03:14 +0200

    make test more thorough

--- tests/test_connectdb.py
+++ tests/test_connectdb.py
@@ -17,10 +17,13 @@ async def db_connect():
     if os.getenv('CI') is None:
         load_dotenv('.env.test')
 
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    database = os.getenv('DB_NAME')
     user = os.getenv('DB_USER')
     password = os.getenv('DB_PASSWORD')
 
-    con = await asyncpg.connect(host='localhost', port=5432, database='test_db', user=user, password=password)
+    con = await asyncpg.connect(host=host, port=port, database=database, user=user, password=password)
     #await con.execute("DROP TABLE IF EXISTS testdb")
 
     await con.execute("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name TEXT, age INT)")
@@ -30,6 +33,13 @@ async def db_connect():
     await con.execute("INSERT INTO test_table (name, age) VALUES ('Deez', 69)")
 
     types = await con.fetch("SELECT * FROM test_table")
+
+    assert types[0]['name'] == 'Alice'
+    assert types[0]['age'] == 22
+    assert types[1]['name'] == 'Bob'
+    assert types[1]['age'] == 72
+    assert types[2]['name'] == 'Deez'
+    assert types[2]['age'] == 69
 
     print(types)
 
