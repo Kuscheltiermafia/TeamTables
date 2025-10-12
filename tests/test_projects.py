0b88ea425856e5b274518b23a59d81b57021e410
Morgon <moritz.olthoff@gmail.com>
2025-10-12 22:06:22 +0200

    Workflow fix attempt 12

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -2,7 +2,6 @@ import pytest
 
 import backend.data_management.pool_handler
 import backend.user_management.pool_handler
-from backend.data_management.project_handler import create_project, get_project_name, get_team_projects
 
 @pytest.mark.data_db
 @pytest.mark.asyncio
@@ -16,6 +15,8 @@ async def setup_project():
 
     await backend.data_management.pool_handler.init_data_pool()
     await backend.user_management.pool_handler.init_user_pool()
+
+    from backend.data_management.project_handler import create_project, get_project_name, get_team_projects
 
     project_id = await create_project(project_name=project_name, owner_id=1, team_id=team_id)
     db_project_name = await get_project_name(project_id)
