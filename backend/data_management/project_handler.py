0989672520a8b95a98072d8fe10201b8b24eda84
CrAzyA22 <klemm.anton@outlook.de>
2025-11-02 15:00:24 +0100

    Fixed test_projects.py

--- backend/data_management/project_handler.py
+++ backend/data_management/project_handler.py
@@ -6,7 +6,7 @@ async def create_project(user_connection:Connection, data_connection:Connection,
     project_id = str(uuid.uuid4())
 
     await data_connection.execute(f'CREATE SCHEMA "{project_id}"')
-    await user_connection.execute('INSERT INTO projects (project_id, project_name, owner_id, team_id) VALUES ($1, $2, $3, $4)', project_id, project_name, owner_id, team_id)
+    await user_connection.execute('INSERT INTO projects (project_id, project_name, owner_id) VALUES ($1, $2, $3)', project_id, project_name, owner_id)
     return str(project_id)
 
 
@@ -17,7 +17,3 @@ async def get_project_name(user_connection:Connection, project_id: str) -> str |
         return result['project_name']
     else:
         return None
-
-async def get_team_projects(user_connection:Connection, team_id: int):
-    results = await user_connection.fetch('SELECT project_id, project_name FROM projects WHERE team_id = $1', team_id)
-    return results
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -2,7 +2,7 @@ import pytest
 
 from conftest import user_db_transaction
 from backend.user_management.team_handler import create_team
-from backend.data_management.project_handler import create_project, get_project_name, get_team_projects
+from backend.data_management.project_handler import create_project, get_project_name
 from backend.user_management.user_handler import create_user
 
 
@@ -21,10 +21,9 @@ async def test_setup_project(user_db_transaction, data_db_transaction):
         firstName="Project"
     )
 
-    team_id = await create_team(user_connection=user_db_transaction, team_name= "Project Testers")
+    team_id = await create_team(user_connection=user_db_transaction, team_name="Project Team")
 
     project_id = await create_project(user_connection=user_db_transaction, data_connection=data_db_transaction, project_name=project_name, owner_id=user_id, team_id=team_id)
     print(f"Created project with ID: {project_id}")
     db_project_name = await get_project_name(user_connection=user_db_transaction, project_id=project_id)
     assert db_project_name == project_name
-    assert await get_team_projects(user_connection=user_db_transaction ,team_id=team_id) == [(project_id, project_name)]
